7cbaa4fa2625184763e24210d39ab1c60cac6da5
Sioncovy <1298184727@qq.com>
2022-09-13 23:28:10 +0800

    ✨ feat: btn&input新增类型检查和事件绑定

--- im-client/src/components/button/button.tsx
+++ im-client/src/components/button/button.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect } from "react";
 
-export default function button(props: any) {
+interface propsType {
+  type?: string;
+  onClick?: Function;
+  children?: string;
+  size?: "small" | "medium" | "large";
+  style?: string;
+}
+
+export default function button(props: propsType) {
   // console.log(props);
   let { children: name } = props;
 
   const customStyle = () => {
     const { type, size, style } = props;
+    // 默认样式
     let styleList = ["box-border py-1 rounded"] as string[];
+
+    // 按钮大小
     let sizeClass = "px-6";
     if (size === "small") {
       sizeClass = "px-4";
@@ -14,6 +25,8 @@ export default function button(props: any) {
       sizeClass = "px-8";
     }
     styleList.push(sizeClass);
+
+    // 按钮类型
     let typeClass =
       "border-2 border-gray-200 hover:border-blue-200 hover:text-blue-500";
     if (type === "primary") {
@@ -21,7 +34,10 @@ export default function button(props: any) {
         "border-2 border-blue-300 bg-blue-400 text-slate-50 hover:bg-blue-300 hover:border-blue-200 ";
     }
     styleList.push(typeClass);
-    styleList.push(style);
+
+    // 用户自定义样式
+    if (style) styleList.push(style);
+
     return styleList.join(" ");
   };
 
--- im-client/src/components/input/input.tsx
+++ im-client/src/components/input/input.tsx
@@ -1,16 +1,39 @@
 import React from "react";
 
-export default function input(props: any) {
+interface propsType {
+  type?: "text" | "password";
+  style?: string;
+  text?: string;
+  onChange?: Function;
+}
+
+export default function input(props: propsType) {
   let { text, type: typeName } = props;
   // console.log(props);
   const customStyle = () => {
-    const { style } = props;
+    // 默认样式表
     let styleList = [
       "outline-none rounded border-2 p-1 px-2 hover:border-blue-200 focus:border-blue-200",
     ] as string[];
-    styleList.push(style);
+
+    // 如果用户自定义样式存在则导入其中
+    const { style } = props;
+    if (style) styleList.push(style);
+
     return styleList.join(" ");
   };
+
+  const changeHangle = (e: any) => {
+    const { onChange } = props;
+    onChange?.(e);
+  };
   const styleList = customStyle();
-  return <input type={typeName} placeholder={text} className={styleList} />;
+  return (
+    <input
+      type={typeName ?? "text"}
+      placeholder={text ?? "请输入..."}
+      className={styleList}
+      onChange={changeHangle}
+    />
+  );
 }
