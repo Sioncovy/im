53dc318ec93bbdf834794b3e2bf16465cef5044d
Sioncovy <1298184727@qq.com>
2022-09-19 22:02:51 +0800

    🐞 fix: 修复敏感信息显示

--- im-server/src/server/email/email.module.ts
+++ im-server/src/server/email/email.module.ts
@@ -3,21 +3,19 @@ import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import { emailConfig } from 'src/config/db';
 
 @Module({
   imports: [
     MailerModule.forRoot({
       transport: {
-        host: 'smtp.exmail.qq.com', //邮箱服务器地址
-        port: 465, //服务器端口 默认 465
-        auth: {
-          user: 'mailpen@sioncovy.top',
-          pass: '5J2fhhRhtY2WGqoD',
-        },
+        host: emailConfig.host,
+        port: emailConfig.port,
+        auth: emailConfig.auth,
       },
       preview: true, // 是否开启预览，开启了这个属性，在调试模式下会自动打开一个网页，预览邮件
       defaults: {
-        from: '信笔科技 <mailpen@sioncovy.top>',
+        from: emailConfig.from,
       },
       template: {
         dir: `${process.cwd()}/src/server/email/template`,
