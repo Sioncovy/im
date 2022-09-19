f7989712efba53204baa10d3656a666410361220
Sioncovy <1298184727@qq.com>
2022-09-19 21:50:06 +0800

    ✨ feat: Input组件新增规则校验

--- im-client/src/components/input/input.tsx
+++ im-client/src/components/input/input.tsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface propsType {
   type?: "text" | "password";
   style?: string;
   text?: string;
   onChange?: Function;
+  validate?: Function;
 }
 
 export default function input(props: propsType) {
-  let { text, type: typeName } = props;
+  let { text, type: typeName, validate } = props;
+  const [status, setStatus] = useState(null as unknown as Boolean);
+
   // console.log(props);
   const customStyle = () => {
     // 默认样式表
     let styleList = [
-      "outline-none rounded border-2 p-1 px-2 hover:border-blue-200 focus:border-blue-200",
+      "outline-none rounded border-2 p-2 px-2 text-xs hover:border-blue-200 focus:border-blue-200",
     ] as string[];
 
     // 如果用户自定义样式存在则导入其中
@@ -25,14 +28,31 @@ export default function input(props: propsType) {
 
   const changeHangle = (e: any) => {
     const { onChange } = props;
-    onChange?.(e);
+    const val = e.target.value;
+    onChange?.(val);
+    if (validate) {
+      if (validate(val)) {
+        setStatus(true);
+      } else {
+        setStatus(false);
+      }
+    }
   };
+
   const styleList = customStyle();
+
   return (
     <input
       type={typeName ?? "text"}
       placeholder={text ?? "请输入..."}
-      className={styleList}
+      className={
+        styleList +
+        (status === null
+          ? ""
+          : status === true
+          ? " border-lime-400"
+          : " border-red-400")
+      }
       onChange={changeHangle}
     />
   );
