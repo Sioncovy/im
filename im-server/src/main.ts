2c31e08d385f58ae0b5f29536c17ab344d210ccf
Sioncovy <1298184727@qq.com>
2022-09-23 23:47:19 +0800

    ✨ feat: 修改跨域限制为允许所有跨域

--- im-server/src/main.ts
+++ im-server/src/main.ts
@@ -1,16 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
-// import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // 允许跨域
-  app.enableCors({
-    credentials: true,
-    origin: 'http://localhost:3000',
-  });
+  app.enableCors();
 
   // 全局注册错误的过滤器
   app.useGlobalFilters(new HttpExceptionFilter());
