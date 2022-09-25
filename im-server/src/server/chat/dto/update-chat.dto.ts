bace42e824d00433e68570842c0ac46dc1ca36af
Sioncovy <1298184727@qq.com>
2022-09-26 01:16:09 +0800

    ✨ feat: 优化 chat 类型接口

--- im-server/src/server/chat/dto/update-chat.dto.ts
+++ im-server/src/server/chat/dto/update-chat.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateChatDto } from './create-chat.dto';
 
-export class UpdateChatDto extends PartialType(CreateChatDto) {}
+export class UpdateChatDto extends PartialType(CreateChatDto) {
+  last_msg?: string;
+  last_msg_id?: string;
+  chat_name?: string;
+  last_user_name?: string;
+  last_time?: number;
+  chat_type?: number;
+  message_type?: number;
+  unread_count?: number;
+}
--- im-server/src/server/chat/entities/chat.entity.ts
+++ im-server/src/server/chat/entities/chat.entity.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { v4 as uuidv4 } from 'uuid';
 
 export type ChatDocument = Chat & Document;
 
